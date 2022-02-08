49e6147d02fe99e265f824c234d904a2d7091e8f
fulfordcs <fulfordcs@gmail.com>
2022-02-08 10:14:55 +0000

    Initial commit

--- FindTheWay/Form1.Designer.cs
+++ FindTheWay/Form1.Designer.cs
@@ -28,13 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtYSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtXSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.panelVis = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.panelVis);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtYSize);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtXSize);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnGenerate);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Settings";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // txtYSize
+            // 
+            this.txtYSize.Location = new System.Drawing.Point(65, 29);
+            this.txtYSize.Name = "txtYSize";
+            this.txtYSize.Size = new System.Drawing.Size(100, 20);
+            this.txtYSize.TabIndex = 4;
+            this.txtYSize.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Grid Y size";
+            // 
+            // txtXSize
+            // 
+            this.txtXSize.Location = new System.Drawing.Point(65, 9);
+            this.txtXSize.Name = "txtXSize";
+            this.txtXSize.Size = new System.Drawing.Size(100, 20);
+            this.txtXSize.TabIndex = 2;
+            this.txtXSize.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Grid X size";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(11, 55);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(160, 31);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate Grid";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // panelVis
+            // 
+            this.panelVis.Location = new System.Drawing.Point(4, 22);
+            this.panelVis.Name = "panelVis";
+            this.panelVis.Padding = new System.Windows.Forms.Padding(3);
+            this.panelVis.Size = new System.Drawing.Size(792, 424);
+            this.panelVis.TabIndex = 1;
+            this.panelVis.Text = "Visualisation";
+            this.panelVis.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(219, 17);
+            this.lblStatus.Text = "Choose the grid size then press generate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "The Way";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage panelVis;
+        private System.Windows.Forms.TextBox txtYSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtXSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
     }
 }
 
--- FindTheWay/Form1.cs
+++ FindTheWay/Form1.cs
@@ -16,5 +16,33 @@ namespace FindTheWay
         {
             InitializeComponent();
         }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Generating grid";
+            tabControl1.SelectTab(1);
+
+            // set defaults
+            int x = 10;
+            int y = 10;
+
+            if(
+                int.TryParse(txtXSize.Text, out x) &&
+                int.TryParse(txtYSize.Text, out y))
+            {
+                lblStatus.Text = "Drawing grid";
+            } else
+            {
+                lblStatus.Text = "Invalid text: using default sizes";
+            }
+
+            DrawGrid(x, y);
+        }
+
+
+        public void DrawGrid(int x, int y)
+        {
+
+        }
     }
 }
